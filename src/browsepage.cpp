16b9418085b21f67dfb6c821c8e4f56d1ae67bba
sunmi <cutysunmi@gmail.com>
2017-10-14 23:38:38 +0900

    commit source

---
+++ src/browsepage.cpp
@@ -0,0 +1,6 @@
+#include "browsepage.h"
+
+BrowsePage::BrowsePage()
+{
+
+}
---
+++ src/browsepage.h
@@ -0,0 +1,21 @@
+#ifndef BROWSEPAGE_H
+#define BROWSEPAGE_H
+
+// Qt Lib
+#include <QDateTime>
+#include <QString>
+#include <QUrl>
+#include <QVariant>
+#include <QNetworkCookieJar>
+
+
+
+
+
+class BrowsePage
+{
+public:
+    BrowsePage();
+};
+
+#endif // BROWSEPAGE_H
