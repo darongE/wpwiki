b33eaec4e8640580797323bee3b0bb13cdfe7845
sunmi <cutysunmi@gmail.com>
2017-10-23 21:13:48 +0900

    update source

--- src/browsepage.cpp
+++ src/browsepage.cpp
@@ -16,14 +16,216 @@
 #include "MediaWiki.h"
 #include "work_p.h"
 
-namespace  mediawiki
+namespace mediawiki
 {
 
+class Result
+{
+public:
+    quint32     m_capchaId;
+    QVariant    m_captchaQuestion;
+    QString     m_captchaAnswer;
+};
 
-BrowsePage::BrowsePage()
+class BrowsePagePrivate : public WorkPrivate
+{
+public:
+    BrowsePagePrivate(MediaWiki& mediawiki)
+          : WorkPrivate(mediawiki)
+
+    {
+
+    }
+
+    static int error(const QString& error)
+    {
+        QString tmp = error;
+        int ret     = 0;
+        QStringList list;
+        list    << QStringLiteral("notext")
+                << QStringLiteral("invalidsection")
+                << QStringLiteral("protectedtitle")
+                << QStringLiteral("cantcreate")
+                << QStringLiteral("cantcreateanon")
+                << QStringLiteral("articleexists")
+                << QStringLiteral("noimageredirectanon")
+                << QStringLiteral("noimageredirect")
+                << QStringLiteral("spamdetected")
+                << QStringLiteral("filtered")
+                << QStringLiteral("contenttoobig")
+                << QStringLiteral("noeditanon")
+                << QStringLiteral("noedit")
+                << QStringLiteral("pagedeleted")
+                << QStringLiteral("emptypage")
+                << QStringLiteral("emptynewsection")
+                << QStringLiteral("editconflict")
+                << QStringLiteral("revwrongpage")
+                << QStringLiteral("undofailure");
+
+        ret = list.indexOf(tmp.remove(QChar::fromLatin1('-')));
+
+        if(ret == -1)
+           ret = 0;
+
+        return ret + (int)BrowsePage::TextMissing;
+
+    }
+
+    QUrl                    baseUrl;
+    QMap<QString, QString>  requestParam;
+    Result                  result;
+
+
+
+};
+
+BrowsePage::BrowsePage(MediaWiki& media, QObject* parent)
+          : Work(*new BrowsePagePrivate(media),parent)
 {
 
 }
 
+void BrowsePage::setUndoAfter(int undoafter)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("undoafter")] = QString::number(undoafter;)
+}
+
+void BrowsePage::setUndo(int undo)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("undo")] = QString::number(undo);
+
+}
+
+void BrowsePage::setPrependText(const QString &prependText)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("prependtext")] = prependText;
+    d->requestParam[QStringLiteral("md5")] = QString();
+
+}
+
+void BrowsePage::setAppendText(const QString &appendText)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("appendtext")] = appendText;
+    d->requestParam[QStringLiteral("md5")] = QString();
+
+}
+
+void BrowsePage::setPageName(const QString &pageName)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringList("title")] = pageName;
+
+}
+
+void BrowsePage::setToken(const QString &token)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringList("token")] = token;
+
+}
+
+void BrowsePage::setBaseTimestamp(const QDateTime &baseTimestamp)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("basetimestamp")] = baseTimestamp;
+
+}
+
+void BrowsePage::setStartTimestamp(const QDateTime &startTimestamp)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("starttimestamp")] = startTimestamp;
+}
+
+void BrowsePage::setText(const QString &text)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("text")] = text;
+    d->requestParam[QStringLiteral("md5")] = QString();
+}
+
+void BrowsePage::setRecreate(bool recreate)
+{
+    Q_D(BrowsePage);
+    if(recreate)
+    {
+       d->requestParam[QStringLiteral("recreate")] = QStringLiteral("on");
+       d->requestParameter[QStringLiteral("md5")] = QString();
+    }
+
+
+}
+
+void BrowsePage::setCreateonly(bool createonly)
+{
+    Q_D(BrowsePage);
+    if(createonly)
+    {
+        d->requestParam[QStringLiteral("createonly")] = QStringLiteral("on");
+        d->requestParam[QStringLiteral("md5")] = QString();
+    }
+}
+
+void BrowsePage::setNocreate(bool norecreate)
+{
+    Q_D(BrowsePage);
+    if(norecreate)
+    {
+        d->requestParameter[QStringLiteral("nocreate")] = QStringLiteral("on");
+        d->requestParameter[QStringLiteral("md5")] = QString();
+    }
+}
+
+void BrowsePage::setMinor(bool minor)
+{
+    Q_D(BrowsePage);
+    if(minor)
+        d->requestParameter[QStringLiteral("minor")] = QStringLiteral("on");
+    else
+        d->requestParameter[QStringLiteral("notminor")] = QStringLiteral("on");
+}
+
+void BrowsePage::setSection(const QString &section)
+{
+    Q_D(BrowsePage);
+    d->requestParameter[QStringLiteral("section")] = section;
+}
+
+void BrowsePage::setSummary(const QString &summary)
+{
+    Q_D(BrowsePage);
+    d->requestParam[QStringLiteral("summary")] = summary;
+}
+
+void BrowsePage::setWatchList(BrowsePage::Watchlist watchlist)
+{
+    Q_D(BrowsePage);
+
+    switch(watchlist){
+
+    case BrowsePage::watch:
+        d->requestParam[QStringLiteral("watchlist")]= QString(QStringLiteral("watch"));
+        break;
+    case BrowsePage::unwatch:
+        d->requestParameter[QStringLiteral("watchlist")] = QString(QStringLiteral("unwatch"));
+        break;
+    case BrowsePage::nochange:
+        d->requestParameter[QStringLiteral("watchlist")] = QString(QStringLiteral("nochange"));
+        break;
+    case BrowsePage::preferences:
+        d->requestParameter[QStringLiteral("watchlist")] = QString(QStringLiteral("preferences"));
+        break;
+    }
+}
+
+void BrowsePage::start()
+{
+    Q_D(BrowsePage);
+
+}
 
 }
