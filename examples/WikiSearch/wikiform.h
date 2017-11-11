8401b37cf9f3f1ddd37a2c415c73ec67c3f5e8b9
sunmi <cutysunmi@gmail.com>
2017-11-11 17:32:53 +0900

    update WikiSearch example

--- examples/WikiSearch/wikiform.h
+++ examples/WikiSearch/wikiform.h
@@ -22,17 +22,36 @@ public:
     explicit WikiForm(QWidget *parent = 0);
     ~WikiForm();
 
+
+
 private Q_SLOTS:
 
 
+    // show on basic data
+    // claims, imageinfo, parse, query, wikidata
 
-
-    Ui::WikiForm *ui;
     void on_TextEdit_BasicInfo_textChanged();
+
+    // show raw data
     void on_TextEdit_General_textChanged();
-    void on_Edit_Page_textChanged(const QString &arg1);
+
+    // url
     void on_Edit_Url_textChanged(const QString &arg1);
+    void on_Edit_Page_textChanged(const QString &arg1);
     void on_btn_load_clicked();
+
+private:
+    Ui::WikiForm *ui;
+    MediaWiki*   mediawiki;
+
+
 };
 
 #endif // WIKIFORM_H
+
+
+
+
+
+
+
